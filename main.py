a2e1ef2e3b0b2e37fddfeaa6a9085ad2067de861
Ishan Joshi <ishanjoshi205@gmail.com>
2025-03-24 22:53:29 -0500

    Added basic GUI

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import re
+from tkinter import *
+
 import bs4
 import requests
-import re
 from Levenshtein import ratio
 
 
-def main():
+def main() -> None:
     url = "https://www.cs.wisc.edu/people/faculty-2/"
-    res = requests.get(url)
+    res = requests.get(url, timeout=5)
     soup = bs4.BeautifulSoup(res.text, "html.parser")
 
     faculty_on_website = {}
@@ -42,12 +44,11 @@ def main():
     #         print(f"Extra {faculty} on the website.")
 
     res = requests.get(
-        "https://raw.githubusercontent.com/emeryberger/CSrankings/gh-pages/csrankings.csv"
+        "https://raw.githubusercontent.com/emeryberger/CSrankings/gh-pages/csrankings.csv",
+        timeout=5,
     )
 
-    if res.status_code != 200:
-        print("Error fetching the CSV file.")
-        return
+    res.raise_for_status()
 
     # read the CSV file from the GitHub URL
     csv_data = res.text.splitlines()
@@ -59,13 +60,13 @@ def main():
 
     for line in csv_rows:
         if len(line) != len(csv_header):
-            print("Mismatch in number of columns.")
+            # print("Mismatch in number of columns.")
             continue
 
         if "University of Wisconsin - Madison" not in line[1]:
             continue
 
-        name = ''.join([i for i in line[0] if not i.isdigit()]).strip()
+        name = "".join([i for i in line[0] if not i.isdigit()]).strip()
         website_link = line[2]
         scholar_id = line[3]
 
@@ -75,20 +76,228 @@ def main():
                 "website": website_link,
             }
         else:
-            faculty_on_csrankings[scholar_id]['name'].append(name)
+            faculty_on_csrankings[scholar_id]["name"].append(name)
 
-    print(len(faculty_on_website))
-    print(len(faculty_on_csrankings))
+    # print(len(faculty_on_website))
+    # print(len(faculty_on_csrankings))
 
-    for faculty in faculty_on_website:
-        if not any(ratio(faculty, faculty_name) > 0.7 for scholar_id in faculty_on_csrankings for faculty_name in faculty_on_csrankings[scholar_id]['name']):
-            print(f"Missing {faculty} on CSrankings.")
-    
-    print()
-    
-    for faculty in faculty_on_csrankings:
-        if not any(ratio(faculty_name, website_faculty) > 0.7 for faculty_name in faculty_on_csrankings[faculty]['name'] for website_faculty in faculty_on_website):
-            print(f"Extra {faculty_on_csrankings[faculty]['name']} on CSRankings.")
+    # for faculty in faculty_on_website:
+    #     if not any(
+    #         ratio(faculty, faculty_name) > 0.7
+    #         for scholar_id in faculty_on_csrankings
+    #         for faculty_name in faculty_on_csrankings[scholar_id]["name"]
+    #     ):
+    # print(f"Missing {faculty} on CSrankings.")
+
+    # print()
+
+    # for faculty in faculty_on_csrankings:
+    #     if not any(
+    #         ratio(faculty_name, website_faculty) > 0.7
+    #         for faculty_name in faculty_on_csrankings[faculty]["name"]
+    #         for website_faculty in faculty_on_website
+    #     ):
+    # print(f"Extra {faculty_on_csrankings[faculty]['name']} on CSRankings.")
+
+    ####################
+
+    def on_frame_configure(canvas: Canvas) -> None:
+        """Reset the scroll region to encompass the inner frame."""
+        canvas.configure(scrollregion=canvas.bbox("all"))
+
+    window = Tk()
+    canvas = Canvas(window)
+    main_frame = Frame(window)
+    scrollbar = Scrollbar(window, orient="vertical", command=canvas.yview)
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    scrollbar.pack(side=RIGHT, fill="y")
+    canvas.pack(side=LEFT, fill=BOTH, expand=True)
+    canvas.create_window((0, 0), window=main_frame, anchor="nw")
+
+    main_frame.bind(
+        "<Configure>",
+        lambda _, canvas=canvas: on_frame_configure(canvas),
+    )
+
+    window.title("CSRankings Faculty Checker")
+    # window.geometry("930x600")
+    window.geometry("475x600")
+
+    # missing_label = Label(main_frame, text="Missing Faculty on CSRankings", width=100, font=("Arial", 12), borderwidth=1, relief="solid", pady=20)
+    missing_label = Label(
+        main_frame,
+        text="Missing Faculty on CSRankings",
+        width=50,
+        font=("Arial", 12),
+        borderwidth=1,
+        relief="solid",
+        pady=20,
+    )
+    missing_label.grid(row=0, column=0, columnspan=3)
+
+    name_header = Label(
+        main_frame,
+        text="Name",
+        width=30,
+        borderwidth=1,
+        relief="solid",
+        font=("Arial, 10"),
+        pady=5,
+    )
+    website_header = Label(
+        main_frame,
+        text="Website",
+        width=50,
+        borderwidth=1,
+        relief="solid",
+        font=("Arial, 10"),
+        pady=5,
+    )
+    add_header = Label(
+        main_frame,
+        text="Added to CSRankings",
+        width=20,
+        borderwidth=1,
+        relief="solid",
+        font=("Arial, 10"),
+        pady=5,
+    )
+
+    name_header.grid(row=1, column=0)
+    # website_header.grid(row=1, column=1)
+    # add_header.grid(row=1, column=2)
+    add_header.grid(row=1, column=1)
+
+    for i, faculty in enumerate(faculty_on_website, start=2):
+        if not any(
+            ratio(faculty, faculty_name) > 0.7
+            for scholar_id in faculty_on_csrankings
+            for faculty_name in faculty_on_csrankings[scholar_id]["name"]
+        ):
+            missing_name = Label(
+                main_frame,
+                text=faculty,
+                width=30,
+                borderwidth=1,
+                relief="solid",
+                font=("Arial, 10"),
+                pady=5,
+            )
+            missing_website = Label(
+                main_frame,
+                text=faculty_on_website[faculty]["website"],
+                width=50,
+                font=("Arial, 10"),
+                borderwidth=1,
+                relief="solid",
+                pady=5,
+            )
+            checkbutton_label = Label(
+                main_frame,
+                width=20,
+                borderwidth=1,
+                relief="solid",
+                font=("Arial, 10"),
+                pady=5,
+            )
+            checkbutton_label.pack_propagate(flag=False)
+            add_checkbutton = Checkbutton(checkbutton_label, text="")
+            add_checkbutton.pack()
+
+            missing_name.grid(row=i, column=0)
+            # missing_website.grid(row=i, column=1)
+            # checkbutton_label.grid(row=i, column=2)
+            checkbutton_label.grid(row=i, column=1)
+
+    # extra_label = Label(main_frame, text="Extra Faculty on CSRankings", width=100, font=("Arial", 12), borderwidth=1, relief="solid", pady=20)
+    extra_label = Label(
+        main_frame,
+        text="Extra Faculty on CSRankings",
+        width=50,
+        font=("Arial", 12),
+        borderwidth=1,
+        relief="solid",
+        pady=20,
+    )
+    extra_label.grid(row=i + 1, column=0, columnspan=3)
+
+    extra_name_header = Label(
+        main_frame,
+        text="Name",
+        width=30,
+        borderwidth=1,
+        relief="solid",
+        font=("Arial, 10"),
+        pady=5,
+    )
+    extra_website_header = Label(
+        main_frame,
+        text="Website",
+        width=50,
+        borderwidth=1,
+        relief="solid",
+        font=("Arial, 10"),
+        pady=5,
+    )
+    extra_add_header = Label(
+        main_frame,
+        text="Removed from CSRankings",
+        width=20,
+        borderwidth=1,
+        relief="solid",
+        font=("Arial, 10"),
+        pady=5,
+    )
+
+    extra_name_header.grid(row=i + 2, column=0)
+    # extra_website_header.grid(row=i+2, column=1)
+    # extra_add_header.grid(row=i+2, column=2)
+    extra_add_header.grid(row=i + 2, column=1)
+
+    for j, faculty in enumerate(faculty_on_csrankings, start=i + 3):
+        if not any(
+            ratio(faculty_name, website_faculty) > 0.7
+            for faculty_name in faculty_on_csrankings[faculty]["name"]
+            for website_faculty in faculty_on_website
+        ):
+            extra_name = Label(
+                main_frame,
+                text=faculty_on_csrankings[faculty]["name"],
+                width=30,
+                borderwidth=1,
+                relief="solid",
+                font=("Arial, 10"),
+                pady=5,
+            )
+            extra_website = Label(
+                main_frame,
+                text=faculty_on_csrankings[faculty]["website"],
+                width=50,
+                font=("Arial, 10"),
+                borderwidth=1,
+                relief="solid",
+                pady=5,
+            )
+            extra_checkbutton_label = Label(
+                main_frame,
+                width=20,
+                borderwidth=1,
+                relief="solid",
+                font=("Arial, 10"),
+                pady=5,
+            )
+            extra_checkbutton_label.pack_propagate(flag=False)
+            extra_add_checkbutton = Checkbutton(extra_checkbutton_label, text="")
+            extra_add_checkbutton.pack()
+
+            extra_name.grid(row=j, column=0)
+            # extra_website.grid(row=j, column=1)
+            # extra_checkbutton_label.grid(row=j, column=2)
+            extra_checkbutton_label.grid(row=j, column=1)
+
+    window.mainloop()
+
 
 if __name__ == "__main__":
     main()
